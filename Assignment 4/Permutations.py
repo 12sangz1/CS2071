dd45894446a1e9ec54284a5889fa3c7624d78e42
Dom Farolino <domfarolino@gmail.com>
2016-11-04 09:33:04 -0400

    Docs: some day-of changes

--- Assignment 4/Permutations.py
+++ Assignment 4/Permutations.py
@@ -1,48 +1,50 @@
 '''
 Team Members: Zachary Sang, Dominic Farolino, Kurt Lewis
 
-How to run program: 
+How to run program:
+    * Note - you must run this program with a Python3 interpreter, not Python2
 	1: Run 'python Permutations.py'
-	2: Follow prompt for 'n: ', enter number to permutate (Eg: if want to permutate {1,2,3,4}, enter '4'), and hit the 'Enter' key
+	2: Follow prompt for 'n: ', enter number of items to permute (Eg: if want to permute {1,2,3,4}, enter '4'), and hit the 'Enter' key
 	3: Follow prompt for 'k: ', enter the kth permutation you want, and hit the 'Enter' key
 	4: View results
 
 Sample runs can be found at the end of this program file
 '''
+
 import math
 import sys
 
-#generates a list of incremental numbers up to k
+# Generates a list of incremental numbers up to k
+# O(n) time complexity
 def listGen(n):
-	if n == 0:
-		return []
-	else:
-		return (listGen(n-1) + [n])
+	return list(range(1, n+1))
 
 
-#Function to take kth permutation of objects in permList
-def perm(k, permList,acc=[]):
+# Tail recursive function to take the kth permutation of objects in permList
+# O(n) time complexity
+# O(1) space complexity through tail recursion
+def perm(k, permList, acc=[]):
 
-	#n holds the number of objects remaining to be permutated
+	# n holds the number of objects remaining to be permutated
 	n = len(permList)
 	if n > 0:
-		#get location in set of the next digit
+		# Get location in set of the next digit
 		digitLoc = math.floor((k)/(math.factorial(n-1))) % n
 
-		#use calculated location to grab next value
+		# Use calculated location to grab next value
 		value = permList[digitLoc]
 
-		#repeat for remaining digits
-		return perm(k,permList[:digitLoc]+permList[digitLoc+1:],acc + [value])
-		
+		# Repeat for remaining digits
+		return perm(k, permList[:digitLoc] + permList[digitLoc+1:], acc + [value])
+
 	else:
 		return acc
 
-#get inputs from user
+# Get inputs from user
 N = input("n: ")
 K = input("k: ")
 
-#check user inputs
+# Check user inputs
 if N == "" or K == "":
 	print("Insufficient values given")
 	sys.exit()
@@ -53,7 +55,7 @@ else:
 		print("Given K value out of bounds (max permutations is {})...Exiting!".format(math.factorial(N)))
 		sys.exit()
 
-#output result
+# Output result
 print("Output: " + str(perm(K,listGen(N))))
 
 '''
@@ -78,5 +80,13 @@ D: python Permutations.py
 n: 4
 k: 30
 Given K value out of bounds (max permutations is 24)...exiting!
+
+--------------------
+
+Bonus Questions
+
+    1.) The way we are handling the permutation generation does not actually depend on whether or not we start at the beginning or end of the permutation list so we've solved #1 through the simplicty of our algorithm.
+
+    2.) We chose to submit the project in Python(3) because we get the ability to handle very very large integers for free, and since handling large integers is the main issue when calculating the kth permutation of a very large set (9+ elements) Python handles this no problem, thus we've solved #2 as well.
 
 '''
